6a2417d91f61b4a958c501cefad51f78fd777952
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-04 17:25:48 +0500

    install api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\CheckAbilities;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,10 +14,25 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: 'api/v1',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'abilities' => CheckAbilities::class,
+            'ability' => CheckForAnyAbility::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if (request()->is('api/*') && $e->getPrevious() instanceof ModelNotFoundException) {
+                $model = Str::afterLast($e->getPrevious()->getMessage(), '\\');
+                $model = preg_replace('/[^a-zA-Z]/', '', $model); // Remove non-alphabetic characters
+
+                return response()->json(['message' => $model.' '.__('not found')], 404);
+            }
+
+            if (request()->is('api/*')) {
+                return response()->json(['message' => __('Not Found')], 404);
+            }
+        });
     })->create();
