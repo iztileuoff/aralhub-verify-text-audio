15308fd3bc42959566b774d19cc037dba2381607
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-07 23:56:31 +0500

    add daily quota

--- app/Http/Controllers/Api/V1/Admin/DailyQuotaController.php
+++ app/Http/Controllers/Api/V1/Admin/DailyQuotaController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api\V1\Admin;
 
+use App\Enums\RoleEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Admin\UpdateProfileRequest;
 use App\Http\Resources\V1\Admin\ProfileResource;
 use App\Models\Text;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
@@ -36,10 +38,20 @@ class DailyQuotaController extends Controller
     private function getDailyQuota()
     {
         return Cache::rememberForever(self::CACHE_KEY, function () {
-            return Text::query()
+            $dailyQuotaTextsCount = Text::query()
                 ->whereNull('edit_finished_at')
                 ->orWhereDate('edit_finished_at', today())
                 ->count();
+
+            $usersCount = User::query()
+                ->where('role', RoleEnum::VOLUNTEER->value)
+                ->count();
+
+            if ($usersCount === 0) {
+                return 0;
+            }
+
+            return intdiv($dailyQuotaTextsCount, $usersCount);
         });
     }
 }
