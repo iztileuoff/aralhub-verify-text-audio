ac8b9219e3a96f5602aca668bbbbde81fc62874c
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-11 23:27:24 +0500

    refactor code

--- app/Http/Requests/Api/V1/Verify/StoreSpeakTextRequest.php
+++ app/Http/Requests/Api/V1/Verify/StoreSpeakTextRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Api\V1\Verify;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Log;
 
 class StoreSpeakTextRequest extends FormRequest
 {
@@ -11,6 +12,21 @@ class StoreSpeakTextRequest extends FormRequest
         return [
             'audio' => ['required', 'file', 'mimes:mp3,wav,m4a,ogg,aac'],
         ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        if ($this->hasFile('audio')) {
+            $file = $this->file('audio');
+
+            Log::info('Audio before validation', [
+                'original_name' => $file->getClientOriginalName(),
+                'client_mime' => $file->getClientMimeType(),
+                'detected_mime' => $file->getMimeType(),
+                'extension' => $file->getClientOriginalExtension(),
+                'size' => $file->getSize(),
+            ]);
+        }
     }
 
     public function authorize(): bool
