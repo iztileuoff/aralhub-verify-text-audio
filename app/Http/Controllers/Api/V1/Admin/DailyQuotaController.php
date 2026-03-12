e3369c75584513e1517c2d7ddfac1a07c2b7d625
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-12 12:41:28 +0500

    refactor code

--- app/Http/Controllers/Api/V1/Admin/DailyQuotaController.php
+++ app/Http/Controllers/Api/V1/Admin/DailyQuotaController.php
@@ -47,6 +47,31 @@ class DailyQuotaController extends Controller
             ->whereIn('role', [RoleEnum::EDITOR->value, RoleEnum::SPEAKER->value, RoleEnum::MODERATOR->value])
             ->count();
 
+        $activeEditorsCount = User::query()
+            ->where('role', RoleEnum::EDITOR->value)
+            ->count();
+
+        $activeSpeakersCount  = User::query()
+            ->where('role', RoleEnum::SPEAKER->value)
+            ->count();
+
+        $activeModeratorsCount = User::query()
+            ->where('role', RoleEnum::MODERATOR->value)
+            ->count();
+
+        $audioFinishedTextsCount = Text::query()
+            ->whereNotNull('speak_finished_at')
+            ->count();
+
+        $moderatorFinishedAudiosCount = Text::query()
+            ->whereNotNull('is_correct')
+            ->count();
+
+        $moderatorNotFinishedAudiosCount = Text::query()
+            ->whereNull('is_correct')
+            ->whereNotNull('speak_finished_at')
+            ->count();
+
         $dailyQuotaTextsCount = Cache::rememberForever(self::CACHE_KEY, function () {
             return Text::query()
                 ->whereNull('edit_finished_at')
@@ -55,11 +80,18 @@ class DailyQuotaController extends Controller
         });
 
         return [
+            'users_count' => $usersCount,
+            'active_editors_count' => $activeEditorsCount,
+            'active_speaker_count' => $activeSpeakersCount,
+            'active_moderators_count' => $activeModeratorsCount,
             'texts_count' => $textsCount,
             'edit_finished_texts_count' => $editFinishedTextsCount,
             'edit_not_finished_texts_count' => $editNotFinishedTextsCount,
             'daily_quota_texts_count' => $dailyQuotaTextsCount,
-            'users_count' => $usersCount,
+            'audio_finished_texts_count' => $audioFinishedTextsCount,
+            'audio_not_finished_texts_count' => $editFinishedTextsCount - $audioFinishedTextsCount,
+            'moderator_finished_audios_count' => $moderatorFinishedAudiosCount,
+            'moderator_not_finished_audios_count' => $moderatorNotFinishedAudiosCount,
             'quota_per_user' => $usersCount ? intdiv($dailyQuotaTextsCount, $usersCount) : 0,
         ];
     }
