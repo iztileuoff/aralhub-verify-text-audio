8b65204e30a8490a1781d726d266f6bd713e7ea1
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-18 18:20:03 +0500

    refactor code

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -17,7 +17,7 @@ class TestCommand extends Command
     public function handle(): void
     {
         $texts = Text::query()
-            ->where('file_id', 2)
+            ->where('file_id', 3)
             ->distinct()
             ->get();
 
