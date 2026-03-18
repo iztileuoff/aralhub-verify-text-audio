11723967d8be1b41c1c69531af23c4a1b5f5443c
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-18 18:55:58 +0500

    refactor code

--- app/Http/Controllers/Api/V1/Admin/FinishedCheckTextController.php
+++ app/Http/Controllers/Api/V1/Admin/FinishedCheckTextController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\Admin\AudioCollection;
 use App\Http\Resources\V1\Admin\TextCollection;
+use App\Models\Audio;
 use App\Models\Text;
 use Illuminate\Http\Request;
 
@@ -11,15 +13,13 @@ class FinishedCheckTextController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $texts = Text::query()
-            ->whereNotNull('moderator_finished_at')
-            ->when($request->filled('file_id'), fn ($q) => $q->where('file_id', $request->input('file_id')))
+        $audio = Audio::query()
             ->when($request->filled('speaker_id'), fn ($q) => $q->where('edit_speaker_id', $request->input('speaker_id')))
             ->when($request->filled('moderator_id'), fn ($q) => $q->where('moderator_id', $request->input('moderator_id')))
-            ->with(['editUser', 'editSpeaker', 'moderator'])
+            ->with(['editSpeaker', 'moderator'])
             ->orderByDesc('moderator_finished_at')
             ->paginate($request->input('per_page', 10));
 
-        return new TextCollection($texts);
+        return new AudioCollection($audio);
     }
 }
