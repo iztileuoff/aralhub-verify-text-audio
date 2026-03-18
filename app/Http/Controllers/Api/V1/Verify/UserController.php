cf09c2c239e2014df7d2d076761707f94c6390b2
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-18 14:38:24 +0500

    refactor code

--- app/Http/Controllers/Api/V1/Verify/UserController.php
+++ app/Http/Controllers/Api/V1/Verify/UserController.php
@@ -20,6 +20,7 @@ class UserController extends Controller
             ->when($request->filled('specialization_id'), fn ($q) => $q->search($request->input('specialization_id')))
             ->when(auth()->user()->role === RoleEnum::ADMIN, fn ($q) => $q->where('admin_id', auth()->user()->id))
             ->withCount(['finishedEditTexts', 'finishedSpeakTexts', 'finishedModerationTexts'])
+            ->withCount(['dateFinishedSpeakAudio' =>  fn ($q) => $q->whereDate('speak_finished_at', '=', $date)])
             ->withCount(['todayFinishedEditTexts' => fn ($q) => $q->whereDate('edit_finished_at', '=', $date)])
             ->withCount(['todayFinishedSpeakTexts' => fn ($q) => $q->whereDate('speak_finished_at', '=', $date)])
             ->withCount(['todayFinishedModerationTexts' => fn ($q) => $q->whereDate('moderator_finished_at', '=', $date)])
