1164bd5f41f9b49dfff72620ac208ad6d09c5485
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-19 11:29:45 +0500

    refactor code

--- app/Http/Controllers/Api/V1/Admin/FinishedAudioTextController.php
+++ app/Http/Controllers/Api/V1/Admin/FinishedAudioTextController.php
@@ -12,7 +12,6 @@ class FinishedAudioTextController extends Controller
     public function __invoke(Request $request)
     {
         $texts = Text::query()
-            ->withMax('audio', 'speak_finished_at')
             ->whereNotNull('speak_finished_at')
             ->when($request->filled('file_id'), fn ($q) =>
                 $q->where('file_id', $request->input('file_id'))
@@ -23,7 +22,6 @@ class FinishedAudioTextController extends Controller
                 )
             )
             ->with(['editUser', 'audio.editSpeaker'])
-            ->orderByDesc('audio_max_speak_finished_at') // 👈 magic column
             ->paginate($request->input('per_page', 10));
 
         return new TextCollection($texts);
