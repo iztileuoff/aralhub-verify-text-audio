986fa6a011ccc097d787e95bfbc311fd1bf01921
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-19 11:31:11 +0500

    refactor code

--- app/Models/Text.php
+++ app/Models/Text.php
@@ -140,6 +140,6 @@ class Text extends Model
 
     public function audio(): HasMany
     {
-        return $this->hasMany(Audio::class);
+        return $this->hasMany(Audio::class, 'edit_speaker_id');
     }
 }
