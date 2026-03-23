b6b689c8088362faf2376883d350a4b526f55268
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-23 21:33:15 +0500

    refactor code

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -17,25 +17,33 @@ class TestCommand extends Command
 
     public function handle(): void
     {
-        $copied = 0;
-        $missing = 0;
+//        $copied = 0;
+//        $missing = 0;
+//
+//        Audio::where('is_correct', true)
+//            ->cursor()
+//            ->each(function ($audio) use (&$copied, &$missing) {
+//                $source = $audio->edit_converted_audio_filename;
+//                $destination = 'correct_audio/' . basename($source);
+//
+//                if (Storage::disk('public')->exists($source)) {
+//                    Storage::disk('public')->copy($source, $destination);
+//                    $copied++;
+//                } else {
+//                    $missing++;
+//                }
+//            });
+//
+//        $this->info("Copied: {$copied}");
+//        $this->warn("Missing: {$missing}");
+//        $this->info('Done!');
 
-        Audio::where('is_correct', true)
-            ->cursor()
-            ->each(function ($audio) use (&$copied, &$missing) {
-                $source = $audio->edit_converted_audio_filename;
-                $destination = 'correct_audio/' . basename($source);
+        $totalSamplesCount = Audio::query()
+            ->where('is_correct', true)
+            ->sum('edit_converted_audio_duration');
 
-                if (Storage::disk('public')->exists($source)) {
-                    Storage::disk('public')->copy($source, $destination);
-                    $copied++;
-                } else {
-                    $missing++;
-                }
-            });
+        $durationInSeconds = $totalSamplesCount / 16000;
 
-        $this->info("Copied: {$copied}");
-        $this->warn("Missing: {$missing}");
-        $this->info('Done!');
+        $this->info("Total samples: {$totalSamplesCount} seconds. Duration in {$durationInSeconds} seconds.");
     }
 }
