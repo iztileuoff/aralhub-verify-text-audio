c751642d8cf8496ca043a591ac9bc89248685a8f
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-23 18:48:33 +0500

    refactor code

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -7,6 +7,7 @@ use App\Models\Action;
 use App\Models\Audio;
 use App\Models\Text;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class TestCommand extends Command
 {
@@ -16,54 +17,11 @@ class TestCommand extends Command
 
     public function handle(): void
     {
-        $texts = Text::query()
-            ->where('file_id', 3)
-            ->get()
-            ->unique('transcript_id')
-            ->values();
-
-//        foreach ($texts as $text) {
-//            // 1. Original Transcript (Оригинал как есть)
-//            $originalTranscript = $text->original_transcript;
-//
-//            // 2. Normalized Transcript
-//            // Приводим к нижнему регистру и удаляем финальную точку
-//            $normalizedTranscript = mb_strtolower(trim($originalTranscript, '.?!'));
-//
-//            // 3. Tokenized Transcript
-//            // Разбиваем на слова, затем каждое слово на буквы через пробел, разделяя слова пайпом |
-//            $words = explode(' ', $normalizedTranscript);
-//            $tokenizedParts = array_map(function ($word) {
-//                // mb_str_split корректно разбивает казахские символы (қ, ө, ұ и т.д.)
-//                $chars = mb_str_split($word);
-//
-//                return implode(' ', $chars).' |';
-//            }, $words);
-//
-//            $tokenizedTranscript = implode(' ', $tokenizedParts);
-//
-//            $text->normalized_transcript = $normalizedTranscript;
-//            $text->tokenized_transcript = $tokenizedTranscript;
-//            $text->save();
-//
-//            $this->info('ID: ' . $text->id . '. Success!');
-//        }
-
-        foreach ($texts as $text) {
-//            $audio = Audio::create([
-//                'text_id' => $text->id,
-//                'edit_audio_filename' => $text->edit_audio_filename,
-//                'edit_converted_audio_filename' => $text->edit_converted_audio_filename,
-//                'edit_converted_audio_duration' => $text->edit_converted_audio_duration,
-//                'speak_started_at' => $text->speak_started_at,
-//                'speak_finished_at' => $text->speak_finished_at,
-//                'edit_speaker_id' => $text->edit_speaker_id,
-//                'edit_speaker_gender' => $text->edit_speaker_gender,
-//            ]);
-
-            $text->is_main = true;
-            $text->save();
-        }
+        Audio::cursor()->each(function ($audio) {
+            if (!Storage::disk('public')->exists($audio->edit_converted_audio_filename)) {
+                $this->info($audio->edit_converted_audio_filename . PHP_EOL);
+            }
+        });
 
         $this->info('Success!');
     }
