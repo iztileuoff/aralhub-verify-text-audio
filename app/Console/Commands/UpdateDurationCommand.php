7314cf74e744de63e4b3df7dae305ba47151611d
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-23 21:15:02 +0500

    refactor code

---
+++ app/Console/Commands/UpdateDurationCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Audio;
+use Illuminate\Console\Command;
+
+class UpdateDurationCommand extends Command
+{
+    protected $signature = 'update:duration';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        $path = storage_path('app/processed_results.txt');
+
+        $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($lines as $line) {
+            [$filename, $duration] = explode(';', $line);
+
+            $audio = Audio::where('edit_audio_filename', "audio/{$filename}")->first();
+
+            if ($audio) {
+                $audio->update([
+                    'edit_converted_audio_duration' => (int) $duration,
+                ]);
+            } else {
+                $this->info("Not found: {$filename}" . PHP_EOL);
+            }
+        }
+
+        $this->info('Audio updated successfully.');
+    }
+}
