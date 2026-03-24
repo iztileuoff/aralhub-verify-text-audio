f547093b12e194b9bd5e30dcceb9cf39d841d8f3
RasulbekGit <rasuliztileuov@gmail.com>
2026-03-24 16:50:26 +0500

    refactor code

---
+++ app/Console/Commands/CopyFilesCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Audio;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class CopyFilesCommand extends Command
+{
+    protected $signature = 'copy:files';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        $copied = 0;
+        $missing = 0;
+
+        Audio::whereNull('edit_converted_audio_duration')
+            ->cursor()
+            ->each(function ($audio) use (&$copied, &$missing) {
+                $source = $audio->edit_converted_audio_filename;
+                $destination = 'correct_audio/' . basename($source);
+
+                if (Storage::disk('public')->exists($source)) {
+                    Storage::disk('public')->copy($source, $destination);
+                    $copied++;
+                } else {
+                    $missing++;
+                }
+            });
+
+        $this->info("Copied: {$copied}");
+        $this->warn("Missing: {$missing}");
+        $this->info('Done!');
+    }
+}
--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -17,27 +17,6 @@ class TestCommand extends Command
 
     public function handle(): void
     {
-//        $copied = 0;
-//        $missing = 0;
-//
-//        Audio::where('is_correct', true)
-//            ->cursor()
-//            ->each(function ($audio) use (&$copied, &$missing) {
-//                $source = $audio->edit_converted_audio_filename;
-//                $destination = 'correct_audio/' . basename($source);
-//
-//                if (Storage::disk('public')->exists($source)) {
-//                    Storage::disk('public')->copy($source, $destination);
-//                    $copied++;
-//                } else {
-//                    $missing++;
-//                }
-//            });
-//
-//        $this->info("Copied: {$copied}");
-//        $this->warn("Missing: {$missing}");
-//        $this->info('Done!');
-
         $totalSamplesCount = Audio::query()
             ->where('is_correct', true)
             ->sum('edit_converted_audio_duration');
