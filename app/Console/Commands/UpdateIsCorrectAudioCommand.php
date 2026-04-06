13e9e7fb3a348d33aafd70221c7a49904c66eb92
RasulbekGit <rasuliztileuov@gmail.com>
2026-04-06 11:25:16 +0500

    refactor code

---
+++ app/Console/Commands/UpdateIsCorrectAudioCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Audio;
+use Illuminate\Console\Command;
+
+class UpdateIsCorrectAudioCommand extends Command
+{
+    protected $signature = 'update:is-correct-audio {user_id}';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        $editSpeakerId = $this->argument('user_id');
+
+        $date = now();
+        $moderatorId = 1;
+
+        $count = Audio::query()
+            ->where('edit_speaker_id', $editSpeakerId)
+            ->whereNull('is_correct')
+            ->update([
+                'is_correct' => true,
+                'moderator_id' => $moderatorId,
+                'moderator_started_at' => $date,
+                'moderator_finished_at' => $date,
+            ]);
+
+        $this->info("Successfully updated {$count} audio records for user ID: {$editSpeakerId}");
+    }
+}
