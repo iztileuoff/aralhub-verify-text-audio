80acd8a3c2794a277800c7886915f7bc34f7029d
RasulbekGit <rasuliztileuov@gmail.com>
2026-04-06 11:50:37 +0500

    refactor code

--- app/Console/Commands/UpdateIsCorrectAudioCommand.php
+++ app/Console/Commands/UpdateIsCorrectAudioCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Audio;
+use App\Models\Text;
 use Illuminate\Console\Command;
 
 class UpdateIsCorrectAudioCommand extends Command
@@ -13,21 +14,35 @@ class UpdateIsCorrectAudioCommand extends Command
 
     public function handle(): void
     {
-        $editSpeakerId = $this->argument('user_id');
+//        $editSpeakerId = $this->argument('user_id');
+//
+//        $date = now();
+//        $moderatorId = 1;
+//
+//        $count = Audio::query()
+//            ->where('edit_speaker_id', $editSpeakerId)
+//            ->whereNull('is_correct')
+//            ->update([
+//                'is_correct' => true,
+//                'moderator_id' => $moderatorId,
+//                'moderator_started_at' => $date,
+//                'moderator_finished_at' => $date,
+//            ]);
+//
+//        $this->info("Successfully updated {$count} audio records for user ID: {$editSpeakerId}");
 
-        $date = now();
-        $moderatorId = 1;
-
-        $count = Audio::query()
-            ->where('edit_speaker_id', $editSpeakerId)
+        $audios = Audio::query()
+            ->whereNull('speak_started_at')
             ->whereNull('is_correct')
-            ->update([
-                'is_correct' => true,
-                'moderator_id' => $moderatorId,
-                'moderator_started_at' => $date,
-                'moderator_finished_at' => $date,
-            ]);
+            ->get();
 
-        $this->info("Successfully updated {$count} audio records for user ID: {$editSpeakerId}");
+        foreach ($audios as $audio) {
+            $textId = $audio->text_id;
+            $editSpeakerId = $audio->edit_speaker_id;
+
+            if (Audio::query()->where('text_id', $textId)->where('edit_speaker_id', $editSpeakerId)->count() > 1) {
+                Audio::query()->where('text_id', $textId)->where('edit_speaker_id', $editSpeakerId)->whereNull('is_correct')->update(['is_correct' => false]);
+            }
+        }
     }
 }
