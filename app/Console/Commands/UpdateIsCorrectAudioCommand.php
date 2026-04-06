4e99fdc0c548df9a57e3d02652936d424314b42c
RasulbekGit <rasuliztileuov@gmail.com>
2026-04-06 11:57:07 +0500

    refactor code

--- app/Console/Commands/UpdateIsCorrectAudioCommand.php
+++ app/Console/Commands/UpdateIsCorrectAudioCommand.php
@@ -14,35 +14,21 @@ class UpdateIsCorrectAudioCommand extends Command
 
     public function handle(): void
     {
-//        $editSpeakerId = $this->argument('user_id');
-//
-//        $date = now();
-//        $moderatorId = 1;
-//
-//        $count = Audio::query()
-//            ->where('edit_speaker_id', $editSpeakerId)
-//            ->whereNull('is_correct')
-//            ->update([
-//                'is_correct' => true,
-//                'moderator_id' => $moderatorId,
-//                'moderator_started_at' => $date,
-//                'moderator_finished_at' => $date,
-//            ]);
-//
-//        $this->info("Successfully updated {$count} audio records for user ID: {$editSpeakerId}");
+        $editSpeakerId = $this->argument('user_id');
 
-        $audios = Audio::query()
-            ->whereNull('speak_started_at')
+        $date = now();
+        $moderatorId = 1;
+
+        $count = Audio::query()
+            ->where('edit_speaker_id', $editSpeakerId)
             ->whereNull('is_correct')
-            ->get();
+            ->update([
+                'is_correct' => true,
+                'moderator_id' => $moderatorId,
+                'moderator_started_at' => $date,
+                'moderator_finished_at' => $date,
+            ]);
 
-        foreach ($audios as $audio) {
-            $textId = $audio->text_id;
-            $editSpeakerId = $audio->edit_speaker_id;
-
-            if (Audio::query()->where('text_id', $textId)->where('edit_speaker_id', $editSpeakerId)->count() > 1) {
-                Audio::query()->where('text_id', $textId)->where('edit_speaker_id', $editSpeakerId)->whereNull('is_correct')->update(['is_correct' => false]);
-            }
-        }
+        $this->info("Successfully updated {$count} audio records for user ID: {$editSpeakerId}");
     }
 }
