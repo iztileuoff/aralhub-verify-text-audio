8a13fd0f3e7c50005918db760e5b84a42469e648
RasulbekGit <rasuliztleuov@gmail.com>
2026-06-01 12:59:21 +0500

    feat: add reported texts count filter by file_id in buildDailyQuota method

--- app/Http/Controllers/Api/V1/Admin/DailyQuotaController.php
+++ app/Http/Controllers/Api/V1/Admin/DailyQuotaController.php
@@ -56,6 +56,12 @@ class DailyQuotaController extends Controller
             ->where('file_id', 8)
             ->count();
 
+        $reportedTextsCount = Text::query()
+            ->where('is_main', true)
+            ->where('file_id', 8)
+            ->where('is_reported', true)
+            ->count();
+
         // $editFinishedTextsCount = Text::query()
         //     ->whereNotNull('edit_finished_at')
         //     ->count();
